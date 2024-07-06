433a5465ceca45f6deae4274c3a775cc5cd27316
PUVHAM <phamquangvu19082005@gmail.com>
2024-07-05 19:32:26 -0700

    Add option video

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import streamlit as st
-from predict import predict_uploaded_image
+from predict import predict_image, predict_video
 
 def main():
     st.title('YOLOv10 Helmet Detection')
 
-    uploaded_file = st.file_uploader("Choose an image to predict", type=['jpg', 'jpeg', 'png'])
+    # Option to choose between uploading an image or predicting from a YouTube video
+    option = st.selectbox("Select input source:", ("Upload Image", "YouTube Video", "Upload Video"))
+
+    if option == "Upload Image":
+        uploaded_file = st.file_uploader("Choose an image to predict", type=['jpg', 'jpeg', 'png'])
+        
+        if st.button('Predict') and uploaded_file is not None:
+            predict_image(uploaded_file)
     
-    if st.button('Predict') and uploaded_file is not None:
-        predict_uploaded_image(uploaded_file)
+    elif option == "YouTube Video":
+        youtube_url = st.text_input("Enter YouTube video URL:", "")
+        
+        if st.button('Predict') and youtube_url:
+            predict_video(youtube_url=youtube_url)
+            
+    elif option == "Upload Video":
+        uploaded_video = st.file_uploader("Choose a video file to predict", type=['mp4', 'avi', 'mov'])
+        
+        if st.button('Predict') and uploaded_video is not None:
+            predict_video(uploaded_video=uploaded_video)
 
 if __name__ == '__main__':
     main()
